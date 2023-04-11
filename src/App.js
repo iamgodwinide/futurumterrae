fce2b875c86a869fbe962d53b9252da3de605a82
Godwin Idemudia <godwinide@gmail.com>
2023-04-11 17:14:16 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function App() {
+
+  function generateRandomBinaryNumber() {
+    let binaryNumber = '';
+    for (let i = 0; i < 11; i++) {
+      binaryNumber += Math.round(Math.random());
+    }
+    return binaryNumber;
+  }
+
+  const handleTwwet = () => {
+    const tweet = `// I have the entered the Golden Age //%0A%0A// Codex: ${generateRandomBinaryNumber()} //%0A%0A%0A%0AComment your Codex below the @FuturumTerrae pinned tweet`;
+    window.open(`https://twitter.com/intent/tweet?text=${tweet}&url=${"https://futurumterrae.xyz"}`, '_blank');
+  }
+
+  return (
+    <div className='main'>
+      <img src='/1.gif'/>
+      <button onClick={handleTwwet}>
+        Enter the Golden Age
+      </button>
+    </div>
+  )
+}
+
+export default App
