d9a5d4dff600d6e2e9bc642e1e7fdb4386a316ca
Godwin Idemudia <godwinide@gmail.com>
2023-04-18 10:08:39 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useRef } from 'react';
 
 function App() {
+  const audio = useRef();
+  const preloadWrap = useRef();
+  const gifImg = useRef();
+  const startButton = useRef();
 
   function generateRandomBinaryNumber() {
     let binaryNumber = '';
@@ -15,8 +20,30 @@ function App() {
     window.open(`https://twitter.com/intent/tweet?text=${tweet}&url=${"https://futurumterrae.com"}`, '_blank');
   }
 
+  const handlePlay = () => {
+    startButton.current.style.display = "none";
+    gifImg.current.style.visibility = "visible";
+    audio.current.play();
+  }
+
+  const handleEnd = () => {
+    preloadWrap.current.style.display = "none";
+  }
+
   return (
     <div className='main'>
+      <div className="preloader" ref={preloadWrap}>
+        <button
+        ref={startButton}
+        onClick={handlePlay}
+        >Click To Start</button>
+        <img ref={gifImg} src='/2.gif'/>
+        <audio 
+        src='/ElevenLabs_2023-04-18T05_35_32.000Z_Josh_bCLiPIexUeHOI6FuOxom.mp3' 
+        ref={audio}
+        onEnded={handleEnd}
+        preload='auto'/>
+      </div>
       <img src='/1.gif'/>
       <button onClick={handleTwwet}>
         Enter the Golden Age
